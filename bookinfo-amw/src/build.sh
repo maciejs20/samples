87121d8bcf4f1dfc26ca547c65b1ba2cbe7d34f4
Maciej Szulc <mszulc@vp.pl>
2022-01-04 12:35:19 +0100

    bi

--- bookinfo-amw/src/build.sh
+++ bookinfo-amw/src/build.sh
@@ -15,6 +15,10 @@ docker push ${REPO}/${IMG}
 cd ..
 
 cd reviews
+
+docker run --rm -u root -v "$(pwd)":/home/gradle/project -w /home/gradle/project gradle:4.8.1 gradle clean build
+pushd reviews-wlpcfg
+
 IMG="examples-bookinfo-reviews-v1:1.0"
 docker build --build-arg service_version=v1 -t ${IMG} .
 docker tag ${IMG} ${REPO}/${IMG}
@@ -29,3 +33,4 @@ IMG="examples-bookinfo-reviews-v3:1.0"
 docker build --build-arg service_version=v3 -t ${IMG} .
 docker tag ${IMG} ${REPO}/${IMG}
 docker push ${REPO}/${IMG}
+popd
